44bc83ff8db26af0c0a40f41059627f1fa19bb07
DariaNosova <73750782+DariaNosova@users.noreply.github.com>
2020-12-02 00:22:01 +0200

    Add files via upload

---
+++ new 1.php
@@ -0,0 +1,23 @@
+<?php	
+
+	$numbers=array([], []);
+	$number=array(
+	array(1,2,3),
+	array(3,4,5)
+);
+	for ($row = 0; $row < 5; $row++) { 
+		echo "<br/>"; 
+		for ($col = 0; $col < 10; $col++) {
+			 $array_rand[$row][$col] = mt_rand(0, 100);
+			echo '	|	';	
+		
+	}
+	}
+	
+?>
+	
+	
+
+	
+	
+  
